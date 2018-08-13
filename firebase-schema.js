01b26cd321ca6f54fe3734efde745a35ffa48fc1
Vardaan Aashish <VardaanAashish@logicmanager.com>
2018-08-13 11:07:53 -0400

    Init

---
+++ firebase-schema.js
@@ -0,0 +1,58 @@
+/*
+No perfect, always tradeoffs
+Keep Shallow
+Repeat data is OK
+
+Home
+    All Ducks
+
+Profile
+    User Info
+    User Ducks
+
+Replies
+    Specific Duck
+    Ducks Replies
+*/
+
+/users
+  uid
+    name
+    uid
+    avatar
+
+/ducks
+  duckId
+    avatar
+    duckId
+    name
+    text
+    timestamp
+    uid (of duck author)
+
+/likeCount
+  duckId
+    0
+
+/usersDucks
+  uid
+    duckId
+      avatar
+      duckId
+      name
+      text
+      timestamp
+      uid (of duck author)
+
+/replies
+  duckId
+    replyId
+      name
+      comment
+      uid
+      timestamp
+      avatar
+
+/usersLikes
+  uid
+    duckId: true
